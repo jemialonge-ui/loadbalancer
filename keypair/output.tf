688c75c9f66f5576a92a4a2a545cbbeb07f3fe5a
Jemi <jaykaylonge@gmail.com>
2026-03-31 02:20:31 -0500

    Revert "Initial setup of load balancing system for two EC2 instances"
    This reverts commit 05e18b63bbad4ae03a364a2deb3f47368cc809da.

--- keypair/output.tf
+++
@@ -1,7 +0,0 @@
-output "key_pair_filename" {
-  value = local_file.ssh_private_key_as_pem.filename
-}
-
-output "key_pairname" {
-  value = aws_key_pair.my_key_pair.key_name
-}
--- s3/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws"  {
-  region = var.region_name
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-}
